5c227e5884c03a83103fdd24a6005ac0e3d2188f
BrunnetteP <80bacg2309062@gmail.com>
2026-09-22 00:50:41 -0600

    Add city selectors and valid flight prices

--- app/schema.sql
+++ app/schema.sql
@@ -36,13 +36,13 @@ INSERT IGNORE INTO Users (username, password, email) VALUES
 ('Carlos', '$2y$10$T3H2ayNlAQ8cJE0KQeJ3eeMH.LuouUi/ojDk7ukitVoGRWUQfijAK', 'carlos@gmail.com');
 
 INSERT IGNORE INTO Flights (origin, destination, departure_date, return_date, price) VALUES
-('Ciudad Juárez', 'Ciudad de México', '2026-10-15', '2026-10-22', 2500.00),
-('Ciudad Juárez', 'Guadalajara', '2026-10-16', '2026-10-23', 1800.50),
-('Ciudad de México', 'Cancún', '2026-10-20', '2026-10-27', 3200.00),
-('Guadalajara', 'Monterrey', '2026-11-05', '2026-11-12', 1200.00),
+('Ciudad Juárez', 'Ciudad de México', '2026-10-15', '2026-10-22', 2400.00),
+('Ciudad Juárez', 'Guadalajara', '2026-10-16', '2026-10-23', 1800.00),
+('Ciudad de México', 'Cancún', '2026-10-20', '2026-10-27', 3000.00),
+('Guadalajara', 'Monterrey', '2026-11-05', '2026-11-12', 1500.00),
 ('Monterrey', 'Tijuana', '2026-11-08', '2026-11-15', 2100.00),
-('Cancún', 'Mérida', '2026-11-12', '2026-11-19', 950.00),
-('Ciudad de México', 'Oaxaca', '2026-11-18', '2026-11-22', 1450.00),
-('Tijuana', 'Guadalajara', '2026-11-25', '2026-12-02', 2750.00);
+('Cancún', 'Mérida', '2026-11-12', '2026-11-19', 1500.00),
+('Ciudad de México', 'Oaxaca', '2026-11-18', '2026-11-22', 1500.00),
+('Tijuana', 'Guadalajara', '2026-11-25', '2026-12-02', 2700.00);
 
-INSERT IGNORE INTO Reservations (user_id, flight_id, seats, total_price, payment_method, payment_status) VALUES (2, 1, 1, 2500.00, 'Tarjeta', 'PAID');
+INSERT IGNORE INTO Reservations (user_id, flight_id, seats, total_price, payment_method, payment_status) VALUES (2, 1, 1, 2400.00, 'Tarjeta', 'PAID');
